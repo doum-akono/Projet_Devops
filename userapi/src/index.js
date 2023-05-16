7c038bb91e705bf3d8e35a79138dec4f22cdb90b
Doum Akono Rudolph <doumakono@gmail.com>
2023-05-16 14:16:27 +0200

    Adding delete route and code for this action

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const ejs = require('ejs');
 const path = require('path');
 const dbConfig = require('../conf/db-config'); // Importer la configuration de la base de données
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 // Définition du moteur de template EJS
@@ -26,6 +28,23 @@ app.get('/users', (req, res) => {
             res.sendStatus(500);
         } else {
             res.render(path.join(__dirname, 'views', 'users.ejs'), { users: results });
+        }
+    });
+});
+app.delete('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const query = 'DELETE FROM users WHERE id = ?';
+
+    dbConfig.query(query, [userId], (error, result) => {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500);
+        } else {
+            if (result.affectedRows > 0) {
+                res.sendStatus(204); // Utilisateur supprimé avec succès, pas de contenu à renvoyer
+            } else {
+                res.sendStatus(404); // Utilisateur non trouvé
+            }
         }
     });
 });
